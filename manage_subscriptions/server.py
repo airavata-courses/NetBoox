b58361f79aee57b99be30eeb1de71eba58d93f1b
knaredla <knaredla@iu.edu>
2018-09-19 09:28:18 -0400

    errors fixed

--- manage_subscriptions/server.py
+++ manage_subscriptions/server.py
@@ -3,33 +3,36 @@ from flask import request, jsonify
 from flask_pymongo import PyMongo
 
 app=Flask(__name__)
-app.config['MONGO_DBNAME']='manage-subscriptions'
-app.config['MONGO_URI']= 'mongodb://<dbuser>:<dbpassword>@ds161312.mlab.com:61312/manage-subscriptions'
-
+app.config['MONGO_DBNAME']='subscribers'
+app.config['MONGO_URI']= 'mongodb://keerthi4308:mlab4308@ds261302.mlab.com:61302/subscribers'
 mongo=PyMongo(app)
 
-@app.route('/manage_subscription/<useremail>', methods=['GET', 'POST', 'UPDATE'])
-def manage_subscription(useremail):
+@app.route('/manage_subscription/add', methods=['POST'])
+def add():
+    sub=mongo.db.users
+    data = request.get_json() 
+    structure={
+        "email": data.get('email'),
+        "phone": data.get('phone'),
+        "subscriptionValid": data.get('subscriptionValid')
+    }
+    new_id=sub.insert(structure)
+    new_data=sub.find_one({'_id':new_id })
+    output={'email':new_data['email'],'phone':new_data['phone'],'subscriptionValid': new_data['subscriptionValid']}
+    return jsonify({'result':output}),200
+    
+@app.route('/manage_subscription/find/<useremail>')
+def find(useremail):
     if request.method == 'GET':
-        query = request.args
-        data = mongo.db.users.find_one(query)
-        return jsonify(data),200
+        new_data = mongo.db.users.find_one({"email": useremail})
+        output={'email':new_data['email'],'phone':new_data['phone'],'subscriptionValid': new_data['subscriptionValid']}
+        return jsonify({'result': output}),200
 
-    data = request.get_json()
-    if request.method == 'POST':
-        if data.get('email', None) is not None:
-            mongo.db.users.insert_one(data)
-            return jsonify({'message': 'Success'}), 200
-        else:
-            return jsonify({'message': 'Bad request'}), 400
-
-    if request.method == 'UPDATE':
-        if data.get('query', {}) != {}:
-            mongo.db.users.update_one(
-                data['query'], {'$set': data.get('payload', {})})
-            return jsonify({'message': 'record updated'}), 200
-        else:
-            return jsonify({'message': 'Bad request'}), 400
+@app.route('/manage_subscription/delete/<email>', methods=['DELETE'])
+def delete(email):
+        data = request.get_json()
+        response = mongo.db.users.delete_one({"email": email})
+        return jsonify({'message': 'record deleted'}), 200
 
 
    
