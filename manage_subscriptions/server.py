fd989aa2fd1ad81dd54744f133de1d7c6e2c07bc
Sagar Suresh Panchal <sagar.panchal11@gmail.com>
2018-09-25 23:41:36 -0400

    Added and endpoint that will Add the users

--- manage_subscriptions/server.py
+++ manage_subscriptions/server.py
@@ -8,11 +8,11 @@ CORS(app)
 app.config['MONGO_DBNAME']='subscribers'
 app.config['MONGO_URI']= 'mongodb://keerthi4308:mlab4308@ds261302.mlab.com:61302/subscribers'
 mongo=PyMongo(app)
+users = mongo.db.users
 
 @app.route('/manage_subscription/add', methods=['POST'])
 def add():
-    sub=mongo.db.users
-    data = request.get_json() 
+    data = request.get_json()
     structure={
         "firstname":data.get('firstname'),
         "lastname":data.get('lastname'),
@@ -20,24 +20,27 @@ def add():
         "phone": data.get('phone'),
         "subscriptionValid": data.get('subscriptionValid')
     }
-    new_id=sub.insert(structure)
-    new_data=sub.find_one({'_id':new_id })
-    output={'firstname':new_data['firstname'],'lastname':new_data['lastname'],'email':new_data['email'],'phone':new_data['phone'],'subscriptionValid': new_data['subscriptionValid']}
-    return jsonify({'result':output}),200
+    new_id=users.insert(structure)
+    new_data=users.find_one({'_id':new_id })
+    if new_data:
+        new_data['_id'] = str(new_data['_id'])
+        return jsonify(new_data),200
+    else:
+        return jsonify({"message": "Not able to add the user"}), 500
     
 @app.route('/manage_subscription/findOneUser/<email>')
 def findOneUser(email):
-    new_data = mongo.db.users.find_one({"email": email})
+    new_data = users.find_one({"email": email})
     if new_data:
         new_data['_id'] = str(new_data['_id'])
         return jsonify(new_data), 200
     else:
-        return jsonify({"message": "User Not Found"}), 200
+        return jsonify({"message": "User Not Found"}), 404
 
 @app.route('/manage_subscription/findAllUsers')
 def findAllUsers():
     new_data = []
-    for user in mongo.db.users.find():
+    for user in users.find():
         user['_id'] = str(user['_id'])
         new_data.append(user)
     return jsonify(new_data), 200
@@ -45,10 +48,12 @@ def findAllUsers():
 
 @app.route('/manage_subscription/deleteUser/<email>')
 def deleteUser(email):
-    if mongo.db.users.delete_one({"email": email}):
+    new_data = users.find_one({"email": email})
+    if new_data:
+        users.delete_one({"_id": new_data['_id']})
         return jsonify({"message": "record deleted"}), 200
     else:
-        return jsonify({"message": "error occured"}), 500
+        return jsonify({"message": "No user record found to delete"}), 404
 
    
 if __name__ == "__main__":
--- manage_subscriptions/test.py
+++ manage_subscriptions/test.py
@@ -1,25 +1,24 @@
 import requests
 import json
 
-# headers = {'Content-type': 'application/json'}
+headers = {"Content-type": "application/json"}
 
-data = {"firstname": "keerthi", "lastname": "naredla","email" : "knaredla@iu.edu", 
-"phone": 8123189999, "subscriptionvalid":"True"}
+data = {"firstname":"Keerthi", "lastname":"Naredla", "email":"knaredla@iu.edu", "phone":8123189999, "subscriptionvalid":True }
 
-# response = requests.post("http://127.0.0.1:5000/manage_subscription/add", headers, data)
-# if(response.status_code == 200):
-#     print("User Added")
-#     print(response.content)
-# else:
-#     print("Error occured")
-#     print(response.status_code)
+response = requests.post("http://127.0.0.1:5000/manage_subscription/add", json.dumps(data), headers=headers)
+if(response.status_code == 200):
+    print("User Added")
+    print(response.content)
+else:
+    print("Error occured: ", response.content)
+    print(response.status_code)
 
 response = requests.get("http://127.0.0.1:5000/manage_subscription/findOneUser/knaredla@iu.edu")
 if(response.status_code==200):
     print("Data fetch Successful from findOneUser endpoint")
     print(response.content)
 else:
-    print("Error occured")
+    print("Error occured: ", response.content)
     print(response.status_code)
 
 response = requests.get("http://127.0.0.1:5000/manage_subscription/findAllUsers")
@@ -27,7 +26,7 @@ if(response.status_code==200):
     print("Data fetch Successful from findAllUsers endpoint")
     print(response.content)
 else:
-    print("Error occured")
+    print("Error occured: ", response.content)
     print(response.status_code)
 
 response = requests.get("http://127.0.0.1:5000/manage_subscription/deleteUser/knaredla@iu.edu")
@@ -35,5 +34,5 @@ if(response.status_code==200):
     print("User Deleted")
     print(response.content)
 else:
-    print("Error occured")
+    print("Error occured: ", response.content)
     print(response.status_code)
