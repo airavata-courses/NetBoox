5612a7af9d65ed4575046644cbd2cf69c8c39647
knaredla <knaredla@iu.edu>
2018-10-22 13:50:00 -0400

    kafka-con

--- server.js
+++ server.js
@@ -33,3 +33,24 @@ app.use('/graphql', express_graphql({
 const server = app.listen(4001, () => {
     console.log(`Express Graphql Server now running on localhost:${server.address().port}/graphql`)
 })
+
+var kafka = require('kafka-node'),
+    Consumer = kafka.Consumer,
+    client = new kafka.Client(),
+    consumer = new Consumer(client,
+        [{ topic: 'posts', offset: 0}],
+        {
+            autoCommit: false
+        }
+    );
+consumer.on('message', function (message) {
+    return updateUserProfile(message);
+});
+
+consumer.on('error', function (err) {
+    console.log('Error:',err);
+})
+
+consumer.on('offsetOutOfRange', function (err) {
+    console.log('offsetOutOfRange:',err);
+})
