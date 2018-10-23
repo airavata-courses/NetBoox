363a6e41eadaba4161d7416a529164c8edd4ac9a
knaredla <knaredla@iu.edu>
2018-10-23 00:14:34 -0400

    minor changes

--- manage_subscriptions/kafkaconsumer.py
+++ manage_subscriptions/kafkaconsumer.py
@@ -5,15 +5,10 @@ import json
 
 
 def kconsumer():
-    print("idhar aaya 1")
     consumer = KafkaConsumer('addSubscriptionProfile')
-    print("idhar aaya 2")
-    # print(consumer)
     for message in consumer:
-        print("it works")
-        print (message.topic)
-        print (json.loads(message.value))
+        # print("it works")
+        # print (message.topic)
+        # print (json.loads(message.value))
         function.addUser(json.loads(message.value))
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
