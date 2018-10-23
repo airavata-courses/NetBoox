ce6e202d4cd5b906b536cb08b6e4ee5ace7c09da
knaredla <knaredla@iu.edu>
2018-10-23 00:08:35 -0400

    Closes #24

--- manage_subscriptions/test.py
+++ manage_subscriptions/test.py
@@ -5,13 +5,13 @@ headers = {"Content-type": "application/json"}
 
 data = {"firstname":"Keerthi", "lastname":"Naredla", "email":"knaredla@iu.edu", "phone":"8123189999", "subscriptionvalid":True }
 
-# response = requests.post("http://127.0.0.1:5000/manage_subscription/addUser", json.dumps(data), headers=headers)
-# if(response.status_code == 200):
-#     print("User Added")
-#     print(response.content)
-# else:
-#     print("Error occured: ", response.content)
-#     print(response.status_code)
+response = requests.post("http://127.0.0.1:4002/manage_subscription/addUser", json.dumps(data), headers=headers)
+if(response.status_code == 200):
+    print("User Added")
+    print(response.content)
+else:
+    print("Error occured: ", response.content)
+    print(response.status_code)
 
 response = requests.get("http://127.0.0.1:4002/manage_subscription/findOneUser/knaredla@iu.edu")
 if(response.status_code==200):
@@ -21,21 +21,21 @@ else:
     print("Error occured: ", response.content)
     print(response.status_code)
 
-# response = requests.get("http://127.0.0.1:5000/manage_subscription/findAllUsers")
-# if(response.status_code==200):
-#     print("Data fetch Successful from findAllUsers endpoint")
-#     print(response.content)
-# else:
-#     print("Error occured: ", response.content)
-#     print(response.status_code)
+response = requests.get("http://127.0.0.1:4002/manage_subscription/findAllUsers")
+if(response.status_code==200):
+    print("Data fetch Successful from findAllUsers endpoint")
+    print(response.content)
+else:
+    print("Error occured: ", response.content)
+    print(response.status_code)
 
-# delete_data = {
-#     "email": "knaredla@iu.edu"
-# }
-# response = requests.post("http://127.0.0.1:5000/manage_subscription/cancelSubscription", json.dumps(delete_data), headers=headers)
-# if(response.status_code==200):
-#     print("Subscription cancelled")
-#     print(response.content)
-# else:
-#     print("Error occured: ", response.content)
-#     print(response.status_code)
+delete_data = {
+    "email": "knaredla@iu.edu"
+}
+response = requests.post("http://127.0.0.1:4002/manage_subscription/cancelSubscription", json.dumps(delete_data), headers=headers)
+if(response.status_code==200):
+    print("Subscription cancelled")
+    print(response.content)
+else:
+    print("Error occured: ", response.content)
+    print(response.status_code)
