872628bb4c6c7dc200e84da5ac03b3c97045ae44
Sagar Suresh Panchal <sagar.panchal11@gmail.com>
2018-11-17 14:29:00 -0500

    Reset the port to 4001

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ consumer.kafkaConsumer()
 /*
  * Start the server
  */
-const server = app.listen(35001, () => {
+const server = app.listen(4001, () => {
     console.log(`Express Graphql Server now running on ${server.address().address} :${server.address().port}/graphql`)
 })
